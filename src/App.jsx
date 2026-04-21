60f0ccd9da2aadc42e9df07b14b77fe3f823d227
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-04-20 20:51:32 -0700

    Rename todoList to todos

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import TodoList from './TodoList'
 import TodoForm from './TodoForm'
 
-const todoList = [
+const todos = [
   {id: 1, title: "review resources"},
   {id: 2, title: "take notes"},
   {id: 3, title: "code out app"},
