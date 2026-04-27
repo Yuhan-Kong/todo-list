1de528935f08c7f6749e98043ebf82c72f0858d5
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-04-27 13:22:22 -0700

    Delete the todos array

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import TodoList from './TodoList'
 import TodoForm from './TodoForm'
 import { useState } from 'react'
 
-const todos = [
-  {id: 1, title: "review resources"},
-  {id: 2, title: "take notes"},
-  {id: 3, title: "code out app"},
-]
+
 
 function App() {
  const [todoList, setTodoList] = useState([]);
