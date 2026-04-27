2e391df6621b4b1a185b58db1893b31e103c960b
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-04-27 14:32:09 -0700

    Hhandle form submit with onSubmit and prevent page reload

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,5 +1,10 @@
 import { useRef } from "react";
 function TodoForm({ onAddTodo }) {
+    const inputRef = useRef();
+
+    const handleAddTodo = (event) => {
+        event.preventDefault();
+    };
     return(
         <form>
             <label htmlFor="todoTitle">Todo</label>
