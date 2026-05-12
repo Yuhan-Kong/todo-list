dfd223952e4e9766cbf6f7f6404b1505908fcd02
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-05-12 16:57:05 -0700

    Create a ternary statement that evaluates isEditing

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -2,14 +2,22 @@ import { useState } from "react";
 import TextInputWithLabel from "../../shared/TextInputWithLabel";
 function TodoListItem({ todo, onCompleteTodo }) {
     const [isEditing, setIsEditing] = useState(false);
-    return(
+    return (
         <li>
-            <input
-                type="checkbox"
-                checked={todo.isCompleted}
-                onChange={() => onCompleteTodo(todo.id)}
-            />
-            {todo.title}
+            {isEditing ? (
+                <TextInputWithLabel
+                    value={todo.title}
+                />
+            ) : (
+                <>
+                    <input
+                        type="checkbox"
+                        checked={todo.isCompleted}
+                        onChange={() => onCompleteTodo(todo.id)}
+                    />
+                    {todo.title}
+                </>
+            )}
         </li>
     );
 }
