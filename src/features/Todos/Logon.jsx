d37df1bc1f2fe217dccf1691cc451ce3d1d2a30c
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-05-18 16:21:27 -0700

    Add authError and isLoggingOn

--- src/features/Todos/Logon.jsx
+++ src/features/Todos/Logon.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 const [email, setEmail] = useState('');
 const [password, setPassword] = useState('');
+const [authError, setAuthError] = useState('');
+const [isLoggingOn, setIsLoggingOn] = useState(false);
 
 function Logon({
     onSetEmail = () => {},
