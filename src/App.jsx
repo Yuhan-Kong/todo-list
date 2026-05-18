604d0ba2eee09b3bf291344869827da5a68e0aea
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-05-18 15:01:48 -0700

    Move addTodo, completeTodo, and updateTodo functions from App.jsx and move them to TodosPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,6 @@ import { useState } from 'react'
 
 function App() {
   
-
- 
-
-function completeTodo(id) {
-  setTodoList(prev => prev.map(todo => {
-    if (todo.id === id) {
-      return {...todo, isCompleted: true};
-    } else {
-      return todo
-    }
-  }))
-}
-
-function updateTodo(editedTodo) {
-  setTodoList(prev => prev.map(todo => {
-    if (todo.id === editedTodo.id) {
-      return {...editedTodo};
-    } else {
-      return todo;
-    }
-  }))
-}
   
   return (
     <div>
--- src/features/Todos/TodosPage.jsx
+++ src/features/Todos/TodosPage.jsx
@@ -1,3 +1,35 @@
 function TodosPage() {
     const [todoList, setTodoList] = useState([]);
+
+    function addTodo(todoTitle) {
+        const newTodo = {
+          id: Date.now(),
+          title: todoTitle,
+          isCompleted: false
+        };
+    
+        setTodoList(previous => [newTodo, ...previous]);
+    
+      }
+
+      function completeTodo(id) {
+        setTodoList(prev => prev.map(todo => {
+          if (todo.id === id) {
+            return {...todo, isCompleted: true};
+          } else {
+            return todo
+          }
+        }))
+      }
+      
+      function updateTodo(editedTodo) {
+        setTodoList(prev => prev.map(todo => {
+          if (todo.id === editedTodo.id) {
+            return {...editedTodo};
+          } else {
+            return todo;
+          }
+        }))
+      }
+      
 }
