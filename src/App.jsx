03d3cfbd644eba04bf0fe8d6d90c978477a7c4c2
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-05-18 15:48:33 -0700

    Remove all todo-related code from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'
-import TodoList from './features/TodoList/TodoList'
-import TodoForm from './features/TodoForm'
-import { useState } from 'react'
+
 
 function App() {
   
   
   return (
     <div>
-      <h1>Todo List</h1>
+      
     </div>
   )
 }
