cf699c39d109983fe47a2bca380d127bca015cc0
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-05-18 22:53:48 -0700

    Import the new Logon component and existing Header component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { useState } from 'react'
 import TodosPage from './features/Todos/TodosPage'
 import Header from './shared/Header'
+import Logon from './features/Logon'
 
 function App() {
-  
+
   
   return (
     <div>
