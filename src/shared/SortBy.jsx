5e9dd09151109587555d6c5d1acaf0dc60561274
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-05-26 16:03:13 -0400

    Create first dropdown sort by

--- src/shared/SortBy.jsx
+++ src/shared/SortBy.jsx
@@ -4,5 +4,18 @@ function SortBy(
     onSortByChange,
     onSortDirectionChange,
   ) {
-
+    return (
+        <div>
+          <label htmlFor="sortBy">Sort by</label>
+      
+          <select
+            id="sortBy"
+            value={sortBy}
+            onChange={(e) => onSortByChange(e.target.value)}
+          >
+            <option value="creationDate">Creation Date</option>
+            <option value="title">Title</option>
+          </select>
+        </div>
+      );
   }
