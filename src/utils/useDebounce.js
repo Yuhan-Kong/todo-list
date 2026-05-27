8eb7f61e9684b8812c7b287dfcccd1bbfe4d4ff8
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-05-27 11:46:22 -0400

    Use useEffect to set up a timer that updates the debounced value after the delay period

--- src/utils/useDebounce.js
+++ src/utils/useDebounce.js
@@ -2,4 +2,9 @@ import { useState, useEffect } from 'react';
 
 function useDebounce(value, delay) {
     const [debouncedValue, setDebouncedValue] = useState(value);
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+    }, [value, delay]);
 }
