19b763457f3143d6af6367f31bff2de4c3f16c31
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-06-02 17:33:40 -0700

    Debug: initial loading state sort and filter

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -30,8 +30,8 @@ export const initialTodoState = {
   filterError: '',
   isTodoListLoading: true,
 
-  sortBy: 'createdAt',
-  sortDirection: 'desc',
+  sortBy: 'createdDate',
+  sortDirection: 'asc',
   filterTerm: '',
 
   dataVersion: 0,
