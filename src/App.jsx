33ae81e504ff7e35caab4bad7b68ffca2a280653
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-06-08 12:39:32 -0700

    Replace the conditional JSX  with a <Routes> structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import './App.css'
 import { Routes, Route } from 'react-router';
 import Header from './shared/Header'
-import TodosPage from './features/Todos/TodosPage'
-import Logon from './features/Logon'
-
 
 function App() {
-  
-
   return (
     <>
       <Header />
-      {isAuthenticated ? <TodosPage /> : <Logon />}
+      <Routes>
+        
+      </Routes>
     </>
   );
 }
