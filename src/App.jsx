7baccd15925797d7c790cbea8324ad911bbb11c0
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-06-08 12:35:28 -0700

    Remove the useAuth import and the isAuthenticated logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import './App.css'
 import Header from './shared/Header'
 import TodosPage from './features/Todos/TodosPage'
 import Logon from './features/Logon'
-import { useAuth } from './contexts/AuthContext'
+
 
 function App() {
-  const { isAuthenticated } = useAuth();
+  
 
   return (
     <>
