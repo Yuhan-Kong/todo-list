53777b39947b0ea68ce05284db2de6fed3cb756f
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-06-08 17:09:45 -0700

    Create a RequireAuth function component that accepts children as props

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -1,3 +1,9 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router';
 import { useAuth } from '../contexts/AuthContext';
+
+function RequireAuth({ children }) {
+
+}
+
+export default RequireAuth;
