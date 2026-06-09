013f456f7d1738019696734e8c46d9098e086630
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-06-09 12:08:22 -0700

    Debug: TextInputWithLabel doesn’t forward refs

--- src/shared/TextInputWithLabel.jsx
+++ src/shared/TextInputWithLabel.jsx
@@ -1,22 +1,21 @@
-function TextInputWithLabel({
-    elementId,
-    labelText,
-    onChange,
-    ref,
-    value,
-  }) {
-    return (
-      <>
-        <label htmlFor={elementId}>{labelText}</label>
-        <input
-          type="text"
-          id={elementId}
-          ref={ref}
-          value={value}
-          onChange={onChange}
-        />
-      </>
-    );
-  }
-  
-  export default TextInputWithLabel;
+import { forwardRef } from "react";
+
+const TextInputWithLabel = forwardRef(function TextInputWithLabel(
+  { elementId, labelText, onChange, value },
+  ref
+) {
+  return (
+    <>
+      <label htmlFor={elementId}>{labelText}</label>
+      <input
+        type="text"
+        id={elementId}
+        ref={ref}
+        value={value}
+        onChange={onChange}
+      />
+    </>
+  );
+});
+
+export default TextInputWithLabel;
