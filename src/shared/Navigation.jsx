647b04403910f98a764b98aa4b80bde4216178b1
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-06-09 10:24:37 -0700

    Use conditional rendering based on isAuthenticated from useAuth()

--- src/shared/Navigation.jsx
+++ src/shared/Navigation.jsx
@@ -2,53 +2,55 @@ import { NavLink } from 'react-router';
 import { useAuth } from '../contexts/AuthContext';
 
 function Navigation() {
-  function navLinkStyle({ isActive }) {
-    return {
-      fontWeight: isActive ? 'bold' : 'normal',
-      textDecoration: isActive ? 'underline' : 'none',
-    };
-  }
+    const { isAuthenticated } = useAuth();
+    
+    function navLinkStyle({ isActive }) {
+        return {
+        fontWeight: isActive ? 'bold' : 'normal',
+        textDecoration: isActive ? 'underline' : 'none',
+        };
+    }
 
-  return (
-    <nav>
-      <ul
-        style={{
-          listStyle: 'none',
-          display: 'flex',
-          gap: '1rem',
-          padding: 0,
-        }}
-      >
-        <li>
-            <NavLink to="/about" style={navLinkStyle}>
-                About
-            </NavLink>
-        </li>
-
-        {isAuthenticated ? (
-          <>
+    return (
+        <nav>
+        <ul
+            style={{
+            listStyle: 'none',
+            display: 'flex',
+            gap: '1rem',
+            padding: 0,
+            }}
+        >
             <li>
-              <NavLink to="/todos" style={navLinkStyle}>
-                Todos
-              </NavLink>
+                <NavLink to="/about" style={navLinkStyle}>
+                    About
+                </NavLink>
             </li>
 
+            {isAuthenticated ? (
+            <>
+                <li>
+                <NavLink to="/todos" style={navLinkStyle}>
+                    Todos
+                </NavLink>
+                </li>
+
+                <li>
+                <NavLink to="/profile" style={navLinkStyle}>
+                    Profile
+                </NavLink>
+                </li>
+            </>
+            ) : (
             <li>
-              <NavLink to="/profile" style={navLinkStyle}>
-                Profile
-              </NavLink>
+                <NavLink to="/login" style={navLinkStyle}>
+                Login
+                </NavLink>
             </li>
-          </>
-        ) : (
-          <li>
-            <NavLink to="/login" style={navLinkStyle}>
-              Login
-            </NavLink>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
-}
+            )}
+        </ul>
+        </nav>
+    );
+    }
 
 export default Navigation;
