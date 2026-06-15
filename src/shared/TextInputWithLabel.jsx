cf18edd283dbcd0e59fd260f254122f4f1c1b606
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-06-15 15:37:35 -0700

    Update TextInputWithLabel.jsx

--- src/shared/TextInputWithLabel.jsx
+++ src/shared/TextInputWithLabel.jsx
@@ -1,20 +1,22 @@
 import { forwardRef } from "react";
+import styles from './TextInputWithLabel.module.css';
 
 const TextInputWithLabel = forwardRef(function TextInputWithLabel(
   { elementId, labelText, onChange, value },
   ref
 ) {
   return (
-    <>
-      <label htmlFor={elementId}>{labelText}</label>
+    <div className={styles.wrapper}>
+      <label htmlFor={elementId} className={styles.label}>{labelText}</label>
       <input
         type="text"
         id={elementId}
         ref={ref}
         value={value}
         onChange={onChange}
+        className={styles.input}
       />
-    </>
+    </div>
   );
 });
 
