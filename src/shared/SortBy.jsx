81d9f5386e83802c36a2d1f174df313436c0abe4
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-06-15 20:33:59 -0700

    Wrap sort and order controls using separate divs in toolbar

--- src/shared/SortBy.jsx
+++ src/shared/SortBy.jsx
@@ -7,31 +7,33 @@ function SortBy({
     onSortDirectionChange,
 }) {
     return (
-        <div className={styles.wrapper}>
-            <label htmlFor="sortBy" className={styles.label}>Sort by</label>
-      
-            <select
-                id="sortBy"
-                value={sortBy}
-                onChange={(e) => onSortByChange(e.target.value)}
-                className={styles.select}
-            >
-                <option value="createdAt">Creation Date</option>
-                <option value="title">Title</option>
-            </select>
+        <>
+            <div className={styles.wrapper}>
+                <label htmlFor="sortBy" className={styles.label}>Sort by</label>
+                <select
+                    id="sortBy"
+                    value={sortBy}
+                    onChange={(e) => onSortByChange(e.target.value)}
+                    className={styles.select}
+                >
+                    <option value="createdAt">Creation Date</option>
+                    <option value="title">Title</option>
+                </select>
+            </div>
 
-            <label htmlFor="sortDirection" className={styles.label}>Order</label>
-
-            <select
-                id="sortDirection"
-                value={sortDirection}
-                onChange={(e) => onSortDirectionChange(e.target.value)}
-                className={styles.select}
-            >
-                <option value="desc">Descending</option>
-                <option value="asc">Ascending</option>
-            </select>
-        </div>
+            <div className={styles.wrapper}>
+                <label htmlFor="sortDirection" className={styles.label}>Order</label>
+                <select
+                    id="sortDirection"
+                    value={sortDirection}
+                    onChange={(e) => onSortDirectionChange(e.target.value)}
+                    className={styles.select}
+                >
+                    <option value="desc">Descending</option>
+                    <option value="asc">Ascending</option>
+                </select>
+            </div>
+        </>
       );
   }
 
